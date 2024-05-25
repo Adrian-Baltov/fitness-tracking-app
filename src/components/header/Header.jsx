a2c757d0a1551e930bb982df190c76fc2cac9bf2
Adrian Baltov <adrian.baltov.a58@learn.telerikacademy.com>
2024-05-25 22:28:58 +0300

    Refactor the header to show a picture and username

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,68 @@
+import { useState, useEffect, useRef } from 'react'
+import { useAuth, useUser } from '../../context'
 import styles from './Header.module.css'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
-    const { container } = styles
+    const { container, dropdownContainer, username, dropdownMenu, usernamePicture, usernameContainer } = styles
+    const [showDropdown, setShowDropdown] = useState(false);
+    const dropdownRef = useRef(null);
+    const { user, logout } = useAuth();
+    const { userData, loading } = useUser();
+
+    const handleLogout = () => {
+        logout();
+    }
+
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
+    };
+
+    const handleClickOutside = (event) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+            setShowDropdown(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("click", handleClickOutside);
+        return () => {
+            document.removeEventListener("click", handleClickOutside);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (user) {
+            setShowDropdown(false);
+        }
+    }, [user]);
 
     return (
         <div className={container}>
             <div>Logo</div>
-            <div>
-                <div>icon</div>
-                <div>name</div>
-            </div>
+            {user ? (
+                <div className={dropdownContainer} ref={dropdownRef}>
+                    <div className={usernameContainer} onClick={toggleDropdown}>
+                        <img src={userData?.profilePicUrl} className={usernamePicture} />
+                        <div className={username}>{userData && userData.username}</div>
+                    </div>
+
+                    {showDropdown && (
+                        <div className={dropdownMenu}>
+                            <NavLink to="/user-profile">Profile</NavLink>
+                            {userData && userData.role === 'admin' && <NavLink to="/admin">Admin Panel</NavLink>}
+                            <button onClick={handleLogout}>Logout</button>
+                        </div>
+                    )}
+                </div>
+            ) : (
+                <div className={dropdownContainer}>
+                    <div className="nav-bar-item">
+
+                        <NavLink to="/auth">Login</NavLink>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
