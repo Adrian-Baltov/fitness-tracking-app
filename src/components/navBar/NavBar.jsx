f3629d559563c03ec98b56114327b09cf791247a
Adrian Baltov <adrian.baltov.a58@learn.telerikacademy.com>
2024-05-25 23:13:26 +0300

    Refactor navBar

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,24 +1,53 @@
-import styles from './NavBar.module.css';
+import React, { useState } from 'react';
 import { NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const NavBar = ({ items, navItemClassName = '', logout }) => {
-    const { container, item: navItem } = styles;
+
+const NavBar = ({ items, logout }) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const filteredItems = items.filter(item => item.title !== 'Logout');
+    const logoutItem = items.find(item => item.title === 'Logout');
 
     return (
-        <div className={container}>
-            {items.map((item) => (
-                <NavLink className={`${navItem}`} key={item.title} to={item.route} onClick={item.title === 'Logout' ? logout : null}>
-                    {item.title}
-                </NavLink>
-            ))}
+        <div
+            className={`flex flex-col items-center bg-base-200 h-full transition-all duration-300 ${isExpanded ? 'w-48' : 'w-16'
+                }`}
+            onMouseEnter={() => setIsExpanded(true)}
+            onMouseLeave={() => setIsExpanded(false)}
+        >
+            <div className="flex-grow">
+                {filteredItems.map((item) => (
+                    <NavLink
+                        className="group flex items-center space-x-4 p-2 my-2 w-full hover:bg-base-300 rounded"
+                        key={item.title}
+                        to={item.route}
+                    >
+                        <span className="text-xl">{item.icon}</span>
+                        <span className={`whitespace-nowrap ${isExpanded ? 'inline' : 'hidden'}`}>{item.title}</span>
+                    </NavLink>
+                ))}
+            </div>
+            {logoutItem && (
+                <div className="mb-4">
+                    <NavLink
+                        className="group flex items-center space-x-4 p-2 my-2 w-full hover:bg-base-300 rounded"
+                        key={logoutItem.title}
+                        to={logoutItem.route}
+                        onClick={logout}
+                    >
+                        <span className="text-xl">{logoutItem.icon}</span>
+                        <span className={`whitespace-nowrap ${isExpanded ? 'inline' : 'hidden'}`}>{logoutItem.title}</span>
+                    </NavLink>
+                </div>
+            )}
         </div>
-    )
-}
-
-NavBar.propTypes = {
-    items: PropTypes.array,
-    navItemClassName: PropTypes.string
+    );
 };
 
-export default NavBar
+NavBar.propTypes = {
+    items: PropTypes.array.isRequired,
+    logout: PropTypes.func.isRequired
+};
+
+export default NavBar;
