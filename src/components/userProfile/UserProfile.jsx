82afe0b95ac117a383e0316ad5a577f888a7a08c
Georgi Burgazliev <nanobat6@gmail.com>
2024-05-28 17:29:40 +0300

    style: make input field bigger

--- src/components/userProfile/UserProfile.jsx
+++ src/components/userProfile/UserProfile.jsx
@@ -212,7 +212,7 @@ const UserProfile = () => {
                                 </div>
                             ) : null}
                             <h1>User Profile</h1>
-                            <label className="input input-bordered flex items-center gap-2">
+                            <label className="input input-bordered flex items-center gap-2 w-full">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
                                 <span className="label-text">Email</span>
                                 <input type="text" className="grow" placeholder="Email" value={userEmail} onChange={(e) => handleOnChangeEmail(e)} />
@@ -221,7 +221,7 @@ const UserProfile = () => {
 
 
                             <div className="divider divider-neutral"> </div>
-                            <label className="input input-bordered flex items-center gap-2">
+                            <label className="input input-bordered flex items-center gap-2 w-full">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
 
                                 <span className="label-text">Username</span>
@@ -230,13 +230,13 @@ const UserProfile = () => {
 
                             </label>
                             <div className="divider divider-neutral"> Personal Information</div>
-                            <label className="input input-bordered flex items-center gap-2">
+                            <label className="input input-bordered flex items-center gap-2 w-full">
                                 <span className="label-text">First Name</span>
                                 <input type="text" className="grow" placeholder="First Name" value={userFirstName} onChange={(e) => handleOnChangeFirstName(e)} />
 
                             </label>
                             <div className="divider divider-neutral"> </div>
-                            <label className="input input-bordered flex items-center gap-2">
+                            <label className="input input-bordered flex items-center gap-2 w-full">
                                 <span className="label-text">Last Name</span>
                                 <input type="text" className="grow" placeholder="Last Name" value={userLastName} onChange={(e) => handleOnChangeUserLastName(e)} />
 
