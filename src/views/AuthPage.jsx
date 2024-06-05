913e2ad4a30a2ccf128b52e88a36003d2ba6f139
NPetranov <nikolay.petranov.a58@learn.telerikacademy.com>
2024-06-05 11:16:35 +0300

    Add styling to AuthPage

--- src/views/AuthPage.jsx
+++ src/views/AuthPage.jsx
@@ -6,18 +6,20 @@ export default function AuthPage() {
     const [isLogin, setIsLogin] = useState(true);
 
     return (
-        <div className="form">
+        <div className="flex w-full justify-center items-center">
+            <div className=" p-6 bg-white justify-center rounded-md">
             {isLogin ? (
                 <>
                     <Login />
-                    <p>Do not have an account? <a href="#" onClick={() => setIsLogin(false)}>Sign Up</a></p>
+                    <p>Do not have an account? <a href="#" onClick={() => setIsLogin(false)} className="font-semibold mt-3">Sign Up</a></p>
                 </>
             ) : (
                 <>
                     <Register />
-                    <p>Already have an account? <a href="#" onClick={() => setIsLogin(true)}>Login</a></p>
+                    <p>Already have an account? <a href="#" onClick={() => setIsLogin(true)} className="font-semibold mt-3">Login</a></p>
                 </>
             )}
+            </div>
         </div>
     );
 }
