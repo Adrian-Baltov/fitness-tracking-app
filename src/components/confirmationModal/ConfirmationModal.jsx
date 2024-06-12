e0654ca7b987f5d0860030c92423a2a9271b4c6c
Adrian Baltov <adrian.baltov.a58@learn.telerikacademy.com>
2024-06-12 21:08:26 +0300

    Refactor the modal to have a general prompt

--- src/components/confirmationModal/ConfirmationModal.jsx
+++ src/components/confirmationModal/ConfirmationModal.jsx
@@ -5,7 +5,7 @@ const ConfirmationModal = ({ isOpen = false, onClose, onConfirm }) => {
 
     return (
         <Dialog visible={isOpen} onHide={onClose}>
-            <h2 className="text-xl mb-4">Are you sure you want to delete this goal?</h2>
+            <h2 className="text-xl mb-4">Are you sure you want to delete this?</h2>
             <div className="flex justify-end space-x-2">
                 <button onClick={onConfirm} className="btn btn-error">Yes</button>
                 <button onClick={onClose} className="btn btn-secondary">No</button>
