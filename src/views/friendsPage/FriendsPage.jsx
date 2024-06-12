c05932c3e9d6e48860f5224fd21f815801b6652c
Adrian Baltov <adrian.baltov.a58@learn.telerikacademy.com>
2024-06-12 21:39:08 +0300

    Center Search users

--- src/views/friendsPage/FriendsPage.jsx
+++ src/views/friendsPage/FriendsPage.jsx
@@ -4,11 +4,11 @@ import './friendsPage.css';
 const FriendsPage = () => {
 
     return (
-        <div className="container flex justify-start mt-64 mb-64 ">
+        <div className="container flex justify-start mt-64 mb-64 " style={{ alignItems: 'center', justifyContent: 'center' }}>
             <div className='border border-white w-1/2 glass flex flex-col h-96 rounded-lg'>
-                  <div className='m-5 flex-grow overflow-y-auto styled-scrollbar '>
-            <SearchAllUsers/>
-         </div>
+                <div className='m-5 flex-grow overflow-y-auto styled-scrollbar '>
+                    <SearchAllUsers />
+                </div>
             </div>
         </div>
     )
